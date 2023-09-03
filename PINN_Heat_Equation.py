c6c215e5f75242f96b5ecd21c47feab07839561e
Chaoyang Mei <136108307+Johnmcy@users.noreply.github.com>
2023-09-03 15:51:43 -0600

    Start training loop

--- PINN_Heat_Equation.py
+++ PINN_Heat_Equation.py
@@ -15,7 +15,7 @@ class PINN_Heat_Equation(nn.Module):
         self.fc4 = nn.Linear(50, 1)
 
     def forward(self, x_t):
-        # Extract the spatial variable x and time variable t from the input tensor x_t
+        # extract the spatial variable x and time variable t from the input tensor x_t
         x, t = x_t[:, 0], x_t[:, 1]
 
         # use cat to merge x and t into a 2D matrix
@@ -26,7 +26,7 @@ class PINN_Heat_Equation(nn.Module):
         x = torch.tanh(self.fc2(x))
         x = torch.tanh(self.fc3(x))
 
-        #  connect the final layer
+        # connect the final layer
         x = self.fc4(x)
 
         # return output
@@ -64,13 +64,35 @@ x_train, t_train = torch.meshgrid(x_train.view(-1), t_train.view(-1))
 x_train = x_train.reshape(-1, 1)
 t_train = t_train.reshape(-1, 1)
 
-# Initialize and add optimizer
+# initialize and add optimizer
 model = PINN_Heat_Equation()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-# Initialize training error list and set the number of training epochs
+# initialize training error list and set the number of training epochs
 train_errors = []
-n_epochs = 50
+epoch_num = 50
+
+# training loop
+for epoch in range(epoch_num):
+    # zero the gradients
+    optimizer.zero_grad()
+
+    # calculate total loss
+    loss = heat_loss(model, x_train, t_train)
+    loss.backward() # calculate gradients
+    loss.step() # update weights
+
+    # store mse loss
+    train_errors.append(loss.item())
+
+    # print train loss per epoch
+    if epoch % 1 == 0:
+        print(f'Epoch {epoch}, Loss {loss.item()}')
+
+
+
+
+
 
 
 
